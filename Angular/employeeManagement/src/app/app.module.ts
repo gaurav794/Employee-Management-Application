b80619884a6829cd1ab142c92081050cf71dd76e
gaurav794 <gauravsharma102000@gmail.com>
2022-10-11 19:27:41 +0530

    feat: Developed Dashboard Structure

--- Angular/employeeManagement/src/app/app.module.ts
+++ Angular/employeeManagement/src/app/app.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { LogoutComponent } from './component/logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [DashboardComponent]
 })
 export class AppModule { }
